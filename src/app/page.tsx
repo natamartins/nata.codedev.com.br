5ee97d68a81a588af666eabba80a400ac9ef2056
natamartins <nata.codedev@gmai.com>
2023-07-14 17:30:31 -0300

    add new style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import AboutMe from "@/components/AboutMe";
 import PostsList from "@/components/PostsList";
 import { getStaticPaths } from "@/utils/getListPosts";
+import { Layout } from "lucide-react";
 
 export default function Home() {
   const markdownFiles: any = getStaticPaths()
 
   return (
     <div className="box_posts-container">
-      <h1>Posts</h1>
+      <h1><Layout size={40} />Posts</h1>
       {markdownFiles.map((post: any) => (
         <PostsList key={post.slug} post={post} />
       ))}
